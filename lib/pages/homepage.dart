40f61cc41a73f20774e417face7d1992bc68d0d2
yogesh salve <you@example.com>
2021-07-26 18:59:16 +0530

    load data from local

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -2,13 +2,34 @@ import 'package:catalog_app/models/products.dart';
 import 'package:catalog_app/widgets/drawer.dart';
 import 'package:catalog_app/widgets/item_widgets.dart';
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:flutter/services.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final productjson =
+        await rootBundle.loadString("assets/files/product.json");
+    final decodedData = jsonDecode(productjson);
+    var productsData = decodedData["products"];
+    print(productsData);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final dummylist = List.generate(20, (index) => ProductModel.items[0]);
+    final dummylist = List.generate(6, (index) => ProductModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("APL ORDER APP"),
